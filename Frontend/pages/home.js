c554509a2282b27bec4fc81730d2d4c22d5d07b2
jsegal <jsegal@iastate.edu>
2020-08-28 12:47:47 -0500

    ✨ 20: Login Page

---
+++ Frontend/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Image, Box, Heading  } from '@chakra-ui/core';
+
+const Home = () => (
+    <Box size="md" m="auto">
+        <Heading>Welcome Home</Heading>
+        <Image src="/images/logo_heads.png" alt="LoCampus" />
+    </Box>
+);
+
+export default Home;

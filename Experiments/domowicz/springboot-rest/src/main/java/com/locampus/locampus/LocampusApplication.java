cf0022320b9599600cacbabacde775a142801a70
domowicz <domowicz@iastate.edu>
2020-08-31 15:17:05 -0500

    ⚗ 44: springboot rest experiment

---
+++ Experiments/domowicz/springboot-rest/src/main/java/com/locampus/locampus/LocampusApplication.java
@@ -0,0 +1,13 @@
+package com.locampus.locampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocampusApplication.class, args);
+	}
+
+}

caff72fbcd602a938c9d95170214a8983e9345b8
jsegal <jsegal@iastate.edu>
2020-08-31 12:13:42 -0500

    ⚗ 15: Add Rest API in Springboot experiment

---
+++ Experiments/jsegal/springboot-rest/src/main/java/com/locampus/locampus/Controller.java
@@ -0,0 +1,19 @@
+package com.locampus.locampus;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/ping")
+    public String live() {
+        return "pong";
+    }
+
+    @GetMapping("/users")
+    public String[] users() {
+        String[] users = {"Alex", "Andrew", "Jonathan", "Andrew"};
+        return users;
+    }
+}

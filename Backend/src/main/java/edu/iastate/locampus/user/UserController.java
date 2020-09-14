93b3c32dd894d6ecb5c40fad912d82ddef921b99
Andrew Yunt <andrew.yunt@gmail.com>
2020-09-14 16:50:44 -0500

    ✨ 67: Init backend users

---
+++ Backend/src/main/java/edu/iastate/locampus/user/UserController.java
@@ -0,0 +1,41 @@
+package edu.iastate.locampus.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/user/register")
+    public void createUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping("/user/{userId}/role")
+    public String getRole(@PathVariable("userId") Integer userId) {
+        return userRepository.getOne(userId).getRole();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping("/user/{userid}/badges")
+    public String[] getBadges(@PathVariable("userId") Integer userId) {
+        return userRepository.getOne(userId).getBadges();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping("/user/{userId}/bio")
+    public String getBio(@PathVariable("userId") Integer userId) {
+        return userRepository.getOne(userId).getBio();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping("/user/{userid}/posts")
+    public Integer[] getPosts(@PathVariable("userid") Integer userId) {
+        return userRepository.getOne(userId).getPosts();
+    }
+}

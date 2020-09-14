fcb0fa14f84ddcb6b1eeaf48f334e66d0051acd9
adeick <adeick@iastate.edu>
2020-09-14 21:23:53 +0000

    👀 70: Updated Badge Header

--- Frontend/pages/badges.js
+++ Frontend/pages/badges.js
@@ -7,12 +7,12 @@ import Container from '../src/components/Shared/Container';
 const Badges = () => (
   <Container>
     {/* <Sidebar /> */}
-    <Box w="94vw" position="absolute" left="6%" px="100px" py="50px">
+    <Box w="100%" h="20%" position="relative" px="100px" py="5px">
         <Text fontSize={[60, 60, 80, 80]} fontFamily="Lexend Deca" display="block">
           Badge Collection
         </Text>
     </Box>
-    <BadgeCase zIndex={99} color="blue.300" w="94%" h="100%" position="relative" left="6%" top="34vh"/>
+    <BadgeCase zIndex={99} color="blue.300" w="100%" h="100%" position="relative" borderRadius="15px"/>
   </Container>
 );
 

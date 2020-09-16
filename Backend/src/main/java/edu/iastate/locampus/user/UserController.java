1e396b66b367b26113232e4d2974d6f76da3ee01
Andrew Yunt <andrew.yunt@gmail.com>
2020-09-16 16:48:00 +0300

    ✨ 67: JSONify UserController

--- Backend/src/main/java/edu/iastate/locampus/user/UserController.java
+++ Backend/src/main/java/edu/iastate/locampus/user/UserController.java
@@ -1,5 +1,7 @@
 package edu.iastate.locampus.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,6 +10,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/user/register")
@@ -18,25 +23,33 @@ public class UserController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/user/{userId}/role")
-    public String getRole(@PathVariable("userId") Integer userId) {
-        return userRepository.getOne(userId).getRole();
+    public ObjectNode getRole(@PathVariable("userId") Integer userId) {
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("role", userRepository.getOne(userId).getRole());
+        return objectNode;
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/user/{userid}/badges")
-    public String[] getBadges(@PathVariable("userId") Integer userId) {
-        return userRepository.getOne(userId).getBadges();
+    public ObjectNode getBadges(@PathVariable("userId") Integer userId) {
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.set("badges", objectMapper.valueToTree(userRepository.getOne(userId).getBadges()));
+        return objectNode;
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/user/{userId}/bio")
-    public String getBio(@PathVariable("userId") Integer userId) {
-        return userRepository.getOne(userId).getBio();
+    public ObjectNode getBio(@PathVariable("userId") Integer userId) {
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("bio", userRepository.getOne(userId).getBio());
+        return objectNode;
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/user/{userid}/posts")
-    public Integer[] getPosts(@PathVariable("userid") Integer userId) {
-        return userRepository.getOne(userId).getPosts();
+    public ObjectNode getPosts(@PathVariable("userid") Integer userId) {
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.set("posts", objectMapper.valueToTree(userRepository.getOne(userId).getPosts()));
+        return objectNode;
     }
 }

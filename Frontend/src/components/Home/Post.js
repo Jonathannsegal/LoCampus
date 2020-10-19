d0c67297369d69ce9e2475a1d0c36d02ad6de9b4
jsegal <jsegal@iastate.edu>
2020-10-19 05:26:22 +0000

    ✨ 113: up/down vote

--- Frontend/src/components/Home/Post.js
+++ Frontend/src/components/Home/Post.js
@@ -5,7 +5,10 @@ import {
   Heading,
   Avatar,
   Stack,
+  Flex,
   Divider,
+  IconButton,
+  ButtonGroup
 } from '@chakra-ui/core';
 
 export default (props) => {
@@ -13,10 +16,24 @@ export default (props) => {
     <>
       <Box borderWidth="1px" rounded="lg" p="10px">
         <Stack isInline spacing={2} align="center">
-          <Avatar size="xs" name={props.author} />
-          <Heading as="h6" size="xs">
-            {props.author}
-          </Heading>
+
+          <Flex flexShrink="0" minW="350px" justifyContent="space-between">
+            <Flex alignItems="center">
+              <Avatar size="xs" name={props.author} />
+              <Heading pl="1em" as="h6" size="xs">
+                {props.author}
+              </Heading>
+            </Flex>
+
+            <Flex>
+              <Stack isInline spacing={2} align="center">
+                <IconButton aria-label="upvote" icon="arrow-up" />
+                <Text>123</Text>
+                <IconButton aria-label="downvote" icon="arrow-down" />
+              </Stack>
+            </Flex>
+          </Flex>
+
         </Stack>
         <Divider />
         <Text>{props.content}</Text>

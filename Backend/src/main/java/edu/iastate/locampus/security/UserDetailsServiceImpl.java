99918b0b914f2b9138a8aff4890243a2f0a409d2
Andrew Yunt <andrew.yunt@gmail.com>
2020-10-23 18:48:37 -0500

    ✨ 114: Integrate roles with Spring Security

--- Backend/src/main/java/edu/iastate/locampus/security/UserDetailsServiceImpl.java
+++ Backend/src/main/java/edu/iastate/locampus/security/UserDetailsServiceImpl.java
@@ -1,13 +1,18 @@
 package edu.iastate.locampus.security;
 
+import edu.iastate.locampus.role.Permission;
+import edu.iastate.locampus.role.RoleRepository;
 import edu.iastate.locampus.user.User;
 import edu.iastate.locampus.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -15,10 +20,19 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByName(username).orElseThrow(() -> new UsernameNotFoundException("User does not exist."));
-        return new UserDetailsImpl(user.getId(), user.getName(), user.getEmail(), user.getPassword(), null);
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        for (String role : user.getRoles()) {
+            for (Permission permission : roleRepository.findByName(role).get().getPermissions()) {
+                authorities.add(new SimpleGrantedAuthority(permission.toString()));
+            }
+        }
+        return new UserDetailsImpl(user.getId(), user.getName(), user.getEmail(), user.getPassword(), authorities);
     }
 }
